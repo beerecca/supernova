fdd44389a61f282712015718c949d4f3b15f82d0
Greggles <gregorynimmo@hotmail.com>
2019-02-08 15:40:12 +0100

    Rename Login page elements

--- supernova.js
+++ supernova.js
@@ -107,7 +107,19 @@
     });
   }
 
+  //Login page
+
+  function renameElements(){
+    //var remember = document.querySelector('label[for="Login_Remember"]');
+    console.log("test");
+    //remember.innerHTML = "Keep me signed in";
+    document.querySelector('label[for="Login_Remember"]').innerHTML = "Keep me signed in";
+    document.querySelector(`div [data-type="passwordlost"]`).innerHTML = "Forgot password?";
+
+  }
+
   setTimeout(() => {
+    renameElements();
     descriptiveClassNames();
 
     createMenu();
@@ -116,6 +128,7 @@
     iconRemove();
     renderName();
     cleanContent();
+
   }, 2000);
   setInterval(() => {
     unhideMenu();
